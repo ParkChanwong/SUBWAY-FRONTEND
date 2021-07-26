778e609beafa20d44f486698b75d73c7e2327bdf
윤지 <yoonhihi97@gmail.com>
2021-07-26 21:10:34 +0900

    fitx findStore html,css,js

--- js/findStore.js
+++ js/findStore.js
@@ -1,9 +1,6 @@
 $(document).ready(function() {
     $('.new_store_slider_wrapper').slick({
-        arrows : true,
-        dots : true, 
+        dots: true,
         dotsClass : "slick-dots",
-        prevArrow : "<button type='button' class='slick-prev'>Previous</button>",		
-		nextArrow : "<button type='button' class='slick-next'>Next</button>"
     });
 });
