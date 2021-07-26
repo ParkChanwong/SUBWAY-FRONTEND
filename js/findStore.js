31d082d567d0931f556f6936d02448eaf5064ee9
윤지 <yoonhihi97@gmail.com>
2021-07-26 20:57:57 +0900

    fix findStore html, css, js

--- js/findStore.js
+++ js/findStore.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
-    $('.new_store_slider_wrapper').slick();
+    $('.new_store_slider_wrapper').slick({
+        arrows : true,
+        dots : true, 
+        dotsClass : "slick-dots",
+        prevArrow : "<button type='button' class='slick-prev'>Previous</button>",		
+		nextArrow : "<button type='button' class='slick-next'>Next</button>"
+    });
 });
