f4a30116763b7c04923b94eae44664e95f3ca44e
신호윤 <shinhoyoon1013@gmail.com>
2021-07-27 17:25:41 +0900

    html, css ing

---
+++ js/tab2.js
@@ -0,0 +1,9 @@
+function tab_fastsub(){
+    $('.tab_fastsub').addClass('active');
+    $('.tab_homesub').removeClass('active');
+}
+
+function tab_homesub(){
+    $('.tab_fastsub').addClass('active');
+    $('.tab_homesub').removeClass('active');
+}
