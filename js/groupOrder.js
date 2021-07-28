fe7b2119d558845969b76332393e41ae3934255b
dece1225 <dece1225@naver.com>
2021-07-28 21:24:51 +0900

    fix codes

--- js/groupOrder.js
+++ js/groupOrder.js
@@ -1,8 +1,13 @@
+$(document).ready(function () {
+    initTmap();
+});
+
 // 페이지가 로딩이 된 후 호출하는 함수입니다.
-function initTmap(){
+function initTmap() {
     var map = new Tmapv2.Map("map_div", { // 지도가 생성될 div
-        width : "100%", // 지도의 넓이
-        height : "260px" // 지도의 높이
+        width: "100%", // 지도의 넓이
+        height: "260px", // 지도의 높이
+        zoom: 12,
+        zoomControl: false
     });
-    zoomControl:false
-} 
+}
