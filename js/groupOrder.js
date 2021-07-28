6379fdac135dd7eb914c0d6ddd5c109bfddd4f13
dece1225 <dece1225@naver.com>
2021-07-28 16:18:59 +0900

    fix mapsize

--- js/groupOrder.js
+++ js/groupOrder.js
@@ -2,6 +2,6 @@
 function initTmap(){
     var map = new Tmapv2.Map("map_div", { // 지도가 생성될 div
         width : "100%", // 지도의 넓이
-        height : "400px" // 지도의 높이
+        height : "260px" // 지도의 높이
     });
 } 
