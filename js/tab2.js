2c1fd1a36481c5cb79175384032f048233cf516b
신호윤 <shinhoyoon1013@gmail.com>
2021-08-01 13:20:33 +0900

    in_cart sum

--- js/tab2.js
+++ js/tab2.js
@@ -1,9 +1,13 @@
 function tab_fastsub(){
     $('.tab_fastsub').addClass('active');
     $('.tab_homesub').removeClass('active');
+    $('.fast_sub_container').css("display", "block");
+    $('.home_sub_container').css("display", "none");
 }
 
 function tab_homesub(){
     $('.tab_fastsub').removeClass('active');
     $('.tab_homesub').addClass('active');
+    $('.fast_sub_container').css("display", "none");
+    $('.home_sub_container').css("display", "block");
 }
