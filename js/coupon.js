25b708950cf3d5183dec6be043160b41256c1590
신호윤 <shinhoyoon1013@gmail.com>
2021-08-08 13:49:10 +0900

    tab js ok

--- js/coupon.js
+++ js/coupon.js
@@ -1,13 +1,13 @@
 function available(){
-    $('#available').css("display", "block");
-    $('#expired').css("display", "none");
-    $('#available').addClass('active');
-    $('#tab_orderTip').removeClass('active');
+    $('.available').css("display", "block");
+    $('.expired').css("display", "none");
+    $('.tab_available').addClass('active');
+    $('.tab_expired').removeClass('active');
 }
 
 function expired(){
-    $('#available').css("display", "none");
-    $('#expired').css("display", "block");
-    $('#expired').addClass('active');
-    $('#available').removeClass('active');
+    $('.available').css("display", "none");
+    $('.expired').css("display", "block");
+    $('.tab_expired').addClass('active');
+    $('.tab_available').removeClass('active');
 }
