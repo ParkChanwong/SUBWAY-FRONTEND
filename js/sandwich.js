1e2a8160caf1af2811d268a909932083bb0b8256
윤지 <yoonhihi97@gmail.com>
2021-08-25 22:28:12 +0900

    fix sandwich html

---
+++ js/sandwich.js
@@ -0,0 +1,3 @@
+TweenMax.to($('.visual.sandwich h2'), 0.7, { ease: Power2.easeInOut, top: 0, opacity: 1 });
+TweenMax.to($('.visual.sandwich p'), 0.7, { ease: Power2.easeOut, top: 0, opacity: 1, delay: 0.3 });
+TweenMax.to($('.visual.sandwich .img01'), 1, { ease: Power4.easeOut, top: 0, opacity: 1, delay: 0.3 });
