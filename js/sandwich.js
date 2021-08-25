cedd7d34d470c55fe914bb93c6857a5abd896f82
윤지 <yoonhihi97@gmail.com>
2021-08-25 23:47:10 +0900

    fix sandwich html, css / add sandwich js

--- js/sandwich.js
+++ js/sandwich.js
@@ -1,3 +1,4 @@
-TweenMax.to($('.visual.sandwich h2'), 0.7, { ease: Power2.easeInOut, top: 0, opacity: 1 });
-TweenMax.to($('.visual.sandwich p'), 0.7, { ease: Power2.easeOut, top: 0, opacity: 1, delay: 0.3 });
-TweenMax.to($('.visual.sandwich .img01'), 1, { ease: Power4.easeOut, top: 0, opacity: 1, delay: 0.3 });
+TweenMax.to('#title', 1, { ease: Power2.easeInOut, top: 0, opacity: 1 });
+TweenMax.to('#title_text', 1, { ease: Power2.easeOut, top: 0, opacity: 1, delay: 0.3 });
+TweenMax.to('.img', 1, { ease: Power4.easeOut, top: 0, opacity: 1, delay: 0.3 });
+
