d83d7c123976ceb4c1644767fc657623a0356075
dece1225 <dece1225@naver.com>
2021-08-26 17:17:49 +0900

    add js slide

---
+++ js/eventPromotion.js
@@ -0,0 +1,16 @@
+$(function(){
+    $('.event_slide').slick({
+        slide: 'div',
+        dots: true,
+        slidesToShow : 1,
+        slidesToScroll : 1,
+        speed : 500,
+        draggable : false,
+        infinite : false,
+    });
+});
+
+TweenMax.to(".event_info", 0.5, {
+    top: 0,
+    opacity: 1,
+});
