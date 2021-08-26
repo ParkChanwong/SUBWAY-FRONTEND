7987510c6e92d973b755245aaa196bb97b8e6be4
윤지 <yoonhihi97@gmail.com>
2021-08-26 12:41:39 +0900

    fix sandwich css

--- js/sandwich.js
+++ js/sandwich.js
@@ -2,3 +2,5 @@ TweenMax.to('#title', 1, { ease: Power2.easeInOut, top: 0, opacity: 1 });
 TweenMax.to('#title_text', 1, { ease: Power2.easeOut, top: 0, opacity: 1, delay: 0.3 });
 TweenMax.to('.img', 1, { ease: Power4.easeOut, top: 0, opacity: 1, delay: 0.3 });
 
+
+
