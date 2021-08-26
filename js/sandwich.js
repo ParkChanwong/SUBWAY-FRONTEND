0496f2f5aac99f6a54ded9693c09b17a659ade5b
윤지 <yoonhihi97@gmail.com>
2021-08-26 16:38:43 +0900

    fix sandwich js

--- js/sandwich.js
+++ js/sandwich.js
@@ -13,9 +13,6 @@ function plusClick(){
     }
 }
 
-function addActive(){
-    
-}
 
 function contryPopup(){
     $('.popUp_back').css("display", "block");
@@ -34,54 +31,3 @@ function closePopup(){
 
 
 
-// $(document).ready(function () {
-// 	gnb();//gnb
-// 	if ($('.sub_header').length > 0) {
-// 		bodyScroll();//body scroll
-// 	}
-
-// 	if ($('div').hasClass('store_list')) {
-// 		$('.store_list ul').mCustomScrollbar();
-// 	}
-
-// 	if ($('div').hasClass('scroll_wrap')) {
-// 		$('.scroll_wrap').mCustomScrollbar({
-// 			scrollInertia: 800,
-// 		});
-// 	}
-// })
-
-// function bodyScroll() {//body scroll
-// 	var subHeader = $('.sub_header')
-// 	var subLogo = subHeader.find('.logo')
-// 	var subTop = subHeader.find('.top')
-// 	var spd = 0.5;
-// 	var eft = Power3.easeOut;
-// 	var subHeaderTop = subHeader.offset().top
-
-// 	scrollBarTo();
-// 	$(window).on('scroll touchmove', function () {//window scrollto
-// 		scrollBarTo();
-// 	})
-
-// 	function scrollBarTo() {
-// 		if ($(window).scrollTop() > subHeaderTop) {
-// 			TweenLite.to(subHeader.css('position', 'fixed'), 0.5, { ease: Power4.easeOut, backgroundColor: '#fff' })
-// 			subHeader.addClass('fixed');
-// 			TweenLite.to(subLogo, spd, { ease: eft, opacity: 1 })
-// 			TweenLite.to(subTop, spd, { ease: eft, opacity: 1 })
-// 		} else {
-// 			TweenLite.to(subHeader.css('position', 'absolute'), 0.5, { ease: Power4.easeOut, backgroundColor: 'transparent' })
-// 			subHeader.removeClass('fixed');
-// 			TweenLite.to(subLogo, spd, { ease: eft, opacity: 0 })
-// 			TweenLite.to(subTop, spd, { ease: eft, opacity: 0 })
-// 		}
-// 	}
-
-// 	subTop.on('click', function () {//top button
-// 		$('html,body').stop(true, false).animate({
-// 			scrollTop: 0
-// 		}, 1000, 'easeInOutQuint')
-// 		return false;
-// 	})
-// }
