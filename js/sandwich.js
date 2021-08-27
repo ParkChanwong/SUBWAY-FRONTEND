feed96d3713def8c3ef38ac8f3ee4af5709f6096
윤지 <yoonhihi97@gmail.com>
2021-08-27 19:03:01 +0900

    fix sandwich html, css, js

--- js/sandwich.js
+++ js/sandwich.js
@@ -1,7 +1,10 @@
+// 메인화면 애니메이션
 TweenMax.to('#title', 1, { ease: Power2.easeInOut, top: 0, opacity: 1 });
 TweenMax.to('#title_text', 1, { ease: Power2.easeOut, top: 0, opacity: 1, delay: 0.3 });
 TweenMax.to('.img', 1, { ease: Power4.easeOut, top: 0, opacity: 1, delay: 0.3 });
 
+
+// 메뉴 상세보기 + 버튼
 function plusClick(){
     let btn = document.getElementById('plus_btn');
     if(btn.className == 'open'){
@@ -13,7 +16,7 @@ function plusClick(){
     }
 }
 
-
+// 팝업 열고 닫기
 function contryPopup(){
     $('.popUp_back').css("display", "block");
     $('.contry_popUp').css("display", "block");
@@ -30,23 +33,24 @@ function closePopup(){
 }
 
 
+// 서브헤더
 var didScroll;
 var lastScrollTop = 0;
 var delta = 5;
-var navbarHeight = 120;
- 
+var navbarHeight = 140;
+
 $(window).scroll(function(event){
   didScroll = true;
- 
+
 });
- 
+
 setInterval(function() {
   if (didScroll) {
     hasScrolled();
     didScroll = false;
   }
 }, 150); 
- 
+
 function hasScrolled() {
   var st = $(this).scrollTop();
   if(Math.abs(lastScrollTop - st) <= delta)
@@ -54,10 +58,28 @@ function hasScrolled() {
   if (st > lastScrollTop && st > navbarHeight){
     $('.sub_header').addClass('nav-up');
   } else {
-    if(st + $(window).height() < $(document).height()) {
+    if(st + $(window).height() < $(document).height()+100) {
       $('.sub_header').removeClass('nav-up');
     }
   }
- 
   lastScrollTop = st;
 }
+
+// active 클래스 부여
+//prodtab
+$(function(){
+  var sBtn = $(".prod_tab_list > li");    //  ul > li 이를 sBtn으로 칭한다. (클릭이벤트는 li에 적용 된다.)
+  sBtn.find("a").click(function(){   // sBtn에 속해 있는  a 찾아 클릭 하면.
+   sBtn.removeClass("active");     // sBtn 속에 (active) 클래스를 삭제 한다.
+   $(this).parent().addClass("active"); // 클릭한 a에 (active)클래스를 넣는다.
+  })
+})
+//menu_list
+$(function(){
+  var sBtn = $(".menu_list > li");    //  ul > li 이를 sBtn으로 칭한다. (클릭이벤트는 li에 적용 된다.)
+  sBtn.find("a").click(function(){   // sBtn에 속해 있는  a 찾아 클릭 하면.
+   sBtn.removeClass("active");     // sBtn 속에 (active) 클래스를 삭제 한다.
+   $(this).parent().addClass("active"); // 클릭한 a에 (active)클래스를 넣는다.
+    // TweenMax.to($('.prod_sandwich_new'), spd, { ease: eft, opacity: 0, scale: 0.8 });
+  })
+})
