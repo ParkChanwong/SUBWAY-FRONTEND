3d871a81df554cf481457392bb3ee91ac7f335f2
ParkChanUng <57166231+ParkChanwong@users.noreply.github.com>
2021-08-29 16:27:47 +0900

    add subHead logo, top fadeIn, out top click event

--- js/franchiseFAQ.js
+++ js/franchiseFAQ.js
@@ -1,4 +1,20 @@
 $(document).ready(function() {
+    let stickyOffset = $('.content').offset();
+    $(window).scroll(function() {
+        if($(document).scrollTop() > stickyOffset.top) {
+            $('.content .logo').fadeIn();
+            $('.content .top').fadeIn();
+        } else {
+            $('.content .logo').fadeOut();
+            $('.content .top').fadeOut();
+        }
+    });
+
+    $(".content .top").click(function() {
+        $('html').animate({ scrollTop : 0 }, 400);
+        return false;
+    });
+
     $('.faq_content li .question').on('click', (e) => {
         if($(e.currentTarget).parent().hasClass('open')) {
             $(e.currentTarget).parent().removeClass('open');
