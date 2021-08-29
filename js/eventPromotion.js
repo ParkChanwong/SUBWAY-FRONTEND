1bed6607156b981bcb33eb4264a99fdc56e3d6d4
dece1225 <dece1225@naver.com>
2021-08-29 19:24:35 +0900

    fix codes

--- js/eventPromotion.js
+++ js/eventPromotion.js
@@ -16,3 +16,20 @@ $(function(){
     });
 });
 
+
+$(function(){	
+		function m_navi_set_on(){
+			$(".eventPromotion_cate_content").addClass("navi_scroll");
+		}
+		function m_navi_set_off(){
+			$(".eventPromotion_cate_content").removeClass("navi_scroll");
+		}
+		
+		if($(window).scrollTop() < 49){
+			m_navi_set_on();
+		}
+		else{
+			m_navi_set_off();
+		}
+        
+});
