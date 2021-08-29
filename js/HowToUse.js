1bfb58ed822963c5266a5095f7970fc4174b8b73
rinlab <75430179+rinlab@users.noreply.github.com>
2021-08-29 19:19:43 +0900

    fix js

---
+++ js/HowToUse.js
@@ -0,0 +1,19 @@
+$(document).ready(function(){
+    $('.visual_slider_content').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade: true,
+        asNavFor: '.bx-pager'
+    });
+    $('.bx-pager').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: '.visual_slider_content',
+        dots: false,
+        centerMode: true,
+        focusOnSelect: true,
+        // centerPadding: '40px',
+        arrows: true,
+    });
+});
