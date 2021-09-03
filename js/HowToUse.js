a931dafa3412a79f777f008f1508ae96065138a5
rinlab <75430179+rinlab@users.noreply.github.com>
2021-09-03 17:44:47 +0900

    fix js

--- js/HowToUse.js
+++ js/HowToUse.js
@@ -3,7 +3,7 @@ $(document).ready(function(){
         slidesToShow: 1,
         slidesToScroll: 1,
         arrows: true,
-        fade: true,
+        fade:  true,
         asNavFor: '.bx-pager'
     });
     $('.bx-pager').slick({
@@ -16,4 +16,30 @@ $(document).ready(function(){
         // centerPadding: '40px',
         arrows: false,
     });
+});
+
+
+$(document).ready(function(){
+
+var pager = document.getElementsByClassName("pager");
+
+function handleClick(event) {
+  if (event.target.classList[1] === "active") {
+    event.target.classList.remove("active");
+  } else {
+    for (var i = 0; i < pager.length; i++) {
+        pager[i].classList.remove("active");
+    }
+    event.target.classList.add("active");
+  }
+}
+
+function init() {
+  for (var i = 0; i < pager.length; i++) {
+    pager[i].addEventListener("click", handleClick);
+  }
+}
+
+init();
+
 });
