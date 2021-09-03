873630e9c331afa3029df1b91c8da59014c5b5cf
윤지 <yoonhihi97@gmail.com>
2021-09-03 22:06:44 +0900

    fix groupMenu css.js

--- js/groupMenu.js
+++ js/groupMenu.js
@@ -33,36 +33,27 @@ function closePopup(){
 
 
 // 서브헤더
-var didScroll;
-var lastScrollTop = 0;
-var delta = 5;
-var navbarHeight = 140;
+$(document).ready(function() {
+  let stickyOffset = $('.sub_header').offset();
+  $(window).scroll(function() {
+      if($(document).scrollTop() > stickyOffset.top) {
+          $('.content .logo').fadeIn();
+          $('.content .top').fadeIn();
+          $('.sub_header').css('background-color', '#ffffff');
+          $('.menu_list li a').css('color', '#999');
+      } else {
+          $('.content .logo').fadeOut();
+          $('.content .top').fadeOut();
+          $('.sub_header').css('background-color', 'transparent');
+          $('.menu_list li a').css('color', '#fff');
+      }
+  });
 
-$(window).scroll(function(event){
-  didScroll = true;
-
+  $(".content .top").click(function() {
+      $('html').animate({ scrollTop : 0 }, 400);
+      return false;
+  });
 });
-
-setInterval(function() {
-  if (didScroll) {
-    hasScrolled();
-    didScroll = false;
-  }
-}, 150); 
-
-function hasScrolled() {
-  var st = $(this).scrollTop();
-  if(Math.abs(lastScrollTop - st) <= delta)
-    return;
-  if (st > lastScrollTop && st > navbarHeight){
-    $('.sub_header').addClass('nav-up');
-  } else {
-    if(st + $(window).height() < $(document).height()+100) {
-      $('.sub_header').removeClass('nav-up');
-    }
-  }
-  lastScrollTop = st;
-}
 
 // active 클래스 부여
 //prodtab
