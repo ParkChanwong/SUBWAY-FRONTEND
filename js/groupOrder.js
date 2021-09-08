5c29fd17623cba89bddbb023345db4a212e7f421
dece1225 <dece1225@naver.com>
2021-09-08 19:43:21 +0900

    fix bugs

--- js/groupOrder.js
+++ js/groupOrder.js
@@ -1,5 +1,33 @@
 $(document).ready(function () {
     initTmap();
+
+    $(".i_star").click(function(){
+        if($(".i_star").hasClass("active")==false){
+            alert("자주찾는 매장으로 등록 완료되었습니다.");
+            $(".i_star").css({
+                "background-color": "#ffc300"
+            });
+            $(this).addClass("active");
+        }else{
+            if(confirm("자주 찾는 매장에서 삭제하시겠습니까?")==true){
+                $(".i_star").removeClass("active");
+                $(".i_star").css({
+                    "background-color": "#dddddd"
+                });
+            }else{
+            }
+        }
+    });
+
+    $(".store_status").click(function(event){
+        $(".popup_back").show();
+        $(".popup_back").css({
+            "display": "flex"
+        });
+    });
+    $(".btn_close").click(function(event){
+        $(".popup_back").hide();
+    })
 });
 
 // 페이지가 로딩이 된 후 호출하는 함수입니다.
@@ -9,42 +37,5 @@ function initTmap() {
         height: "260px", // 지도의 높이
         zoom: 15,
         zoomControl: false
-    });
-}
-
-$(function(){
-    $(".popup_back").css({
-        "display": "none"
-    });
-
-    $(".store_status").click(function(event){
-        $(".popup_back").show();
-    });
-    $(".btn_close").click(function(event){
-        $(".popup_back").css({
-            "display": "none"
-        });
-    })
-});
-
-const bookmark = function(){
-    $(function(){
-        $(".i_star").click(function(){
-            if($(".i_star").hasClass("active")==false){
-                alert("자주찾는 매장으로 등록 완료되었습니다.");
-                $(".i_star").css({
-                    "background-color": "#ffc300"
-                });
-                $(this).addClass("active");
-            }else{
-                if(confirm("자주 찾는 매장에서 삭제하시겠습니까?")==true){
-                    $(".i_star").removeClass("active");
-                    $(".i_star").css({
-                        "background-color": "#dddddd"
-                    });
-                }else{
-                }
-            }
-        });
     });
 }
